9c57e0ec97dd293d5fa0ba4a38b1a29c1656aa36
svetlana012 <svetlanaguseva264@gmail.com>
2026-06-23 19:05:24 +0300

    исправила дз

--- script_6.js
+++ script_6.js
@@ -13,12 +13,7 @@ const str = " javascript is awesome ";
 
 console.log("РЕШЕНИЕ К ЗАДАНИЮ 1:")
 
-function inputStr (input) {
-    let result = String(input ?? "")
-    console.log("Исходная строка: ", result)
-}
-inputStr(str)
-
+console.log("Исходная строка: ", str)
 console.log("Строка без пробелов с краёв: ", str.trim());
 console.log("Строка с верхним регистром: ", str.toUpperCase());
 console.log("Строка с заменой 'A' на '@': ", str.replaceAll("A", "@"));
@@ -37,12 +32,7 @@ const price = 12345.789;
 // 5. Округли до ближайшего десятого вниз и выведи как число, потом снова как строку с "₽" в конце
 console.log("\n\nРЕШЕНИЕ К ЗАДАНИЮ 2:")
 
-function inputPrice (input) {
-    let result = Number(input ?? "")
-    console.log("Исходное число: ", result)
-}
-inputPrice(price)
-
+console.log("Исходное число: ", price)
 console.log("Цена с двумя знаками после запятой: ", price.toFixed(2));
 console.log("Цена с разделителем тысяч: ", price.toLocaleString('ru-RU'));
 console.log("Проверь, является ли число целым: ", Number.isInteger(price));
@@ -60,27 +50,23 @@ console.log("Строку с '₽' в конце: ", price + " " + "₽");
 //}
 console.log("\n\nРЕШЕНИЕ К ЗАДАНИЮ 3:")
 
-function validateUsername(username) {
+function validateUsername(username = "") {    
+    const newStroke = username.trim().toLowerCase()
 
-    let stroke = String(username ?? "")
+    console.log("Исходная строка: ", username)
+    console.log("Строка без пробелов с краёв: ", username.trim());
+    console.log("Строка с нижним регистром: ", username.toLowerCase());
 
-    console.log("Исходная строка: ", stroke)
-    console.log("Строка без пробелов с краёв: ", stroke.trim());
-    console.log("Строка с нижним регистром: ", stroke.toLowerCase());
-
-    if (stroke.length < 4 || stroke.length > 16) {       
-        console.log("Если длина меньше 4 или больше 16 символов — верни false: ", false); 
+    if (newStroke.length < 4 || newStroke.length > 16) {               
         return false;
     }
-    if (!/^[a-zA-Z]/.test(stroke)) {   
-        console.log("Если начинается не с буквы — верни false: ", false);      
+    if (!/^[a-zA-Z]/.test(newStroke)) {                 
         return false;
     }
-    if (stroke.includes(" ")) {
-        console.log("Если содержит пробел внутри — верни false: ", false)
+    if (newStroke.includes(" ")) {        
         return false;
     }
-    console.log("Логин валиден: ", stroke)
+    console.log("Логин валиден: ", newStroke)
     return true;
 }
 
@@ -92,22 +78,21 @@ validateUsername("ab");               // false (слишком короткий)
 
 //Задание 4.
 const rawPhone = "  7 923 456-78-90  ";
-
 // Сделай красивый номер +7 (923) 456-78-90
 // Используй ТОЛЬКО методы строк и конкатенацию!
 // Запрещено: split(), массивы, регулярные выражения
-
 // Подсказка: убери все пробелы и дефисы, потом собирай по частям
 console.log("\n\nРЕШЕНИЕ К ЗАДАНИЮ 4:")
 
+function formatPhoneNumber(rawPhone) {
+    const clean = rawPhone.trim().replaceAll(" ", "").replaceAll("-", "");
+    if (clean.length !== 11) {
+        return "Ошибка: номер должен содержать 11 цифр";
+    }
+    return `+${clean[0]} (${clean.slice(1, 4)}) ${clean.slice(4, 7)}-${clean.slice(7, 9)}-${clean.slice(9, 11)}`;
+}
 console.log("Исходная строка:", rawPhone);
-console.log("Строка без пробелов с краёв: ", rawPhone.trim());
-console.log("После удаления пробелов внутри:", rawPhone.trim().replaceAll(" ", ""));
-console.log("После удаления дефисов:", rawPhone.trim().replaceAll(" ", "").replaceAll("-", ""));
-
-const clean = rawPhone.trim().replaceAll(" ", "").replaceAll("-", "");
-
-console.log("Красивый номер:", `+${clean[0]} (${clean.slice(1, 4)}) ${clean.slice(4, 7)}-${clean.slice(7, 9)}-${clean.slice(9, 11)}`);
+console.log("Красивый номер:", formatPhoneNumber(rawPhone));
 
 //Задача 5.
 const sentence = "Я изучаю JavaScript каждый день";
@@ -118,20 +103,19 @@ const sentence = "Я изучаю JavaScript каждый день";
 console.log("\n\nРЕШЕНИЕ К ЗАДАНИЮ 5:")
 console.log("Исходная строка:", sentence);
 
-function words(sentence) {
-    let wordCount = 0;
-    let isPreviousSpace = true;
+function words(sentence) {    
+
+    let wordCount = 0;    
 
     for (let i = 0; i < sentence.length; i++) {
-        const isCurrentSpace = sentence[i] === ' ';
-
-         if (isPreviousSpace && !isCurrentSpace) {
+        if (sentence[i] === " ") {
             wordCount++;
         }
-        
-        isPreviousSpace = isCurrentSpace;
     }
-    return wordCount
+    if (sentence.length === 0) {
+        return 0;
+    }
+    return wordCount + 1
 }
 console.log(`В предложении ${words(sentence)} слов`)
 
@@ -147,18 +131,24 @@ const fullName = " иванов Сергей Петрович ";
 console.log("\n\nРЕШЕНИЕ К ЗАДАНИЮ 6:")
 console.log("Исходная строка:", fullName);
 
-let name = fullName.trim(); //удаление пробелов по краям
-let firstSpace = name.indexOf(' '); //поиск первой позиции пробелов //6
-let secondSpace = name.indexOf(' ', firstSpace + 1); //поиск второй позиции пробелов //13
-//надо изменить каждое слово отдельно
-const surname = name.slice(0, firstSpace); // "иванов"
-const formatterSurname = surname.charAt(0).toUpperCase() + surname.slice(1).toLowerCase(); //первая заглавная буква, остальные строчные
+function abbreviateName(fullName) {    
 
-const firstName = name.slice(firstSpace + 1, secondSpace); // "Сергей"
-const firstNameInitial = firstName.charAt(0).toUpperCase() + "."; // С.
+    let name = fullName.trim(); //удаление пробелов по краям
+    let firstSpace = name.indexOf(' '); //поиск первой позиции пробелов //6
+    let secondSpace = name.indexOf(' ', firstSpace + 1); //поиск второй позиции пробелов //13
+    //надо изменить каждое слово отдельно
+    const surname = name.slice(0, firstSpace); // "иванов"
+    const formatterSurname = surname.charAt(0).toUpperCase() + surname.slice(1).toLowerCase(); //первая заглавная буква, остальные строчные
 
-const patronymic = name.slice(secondSpace + 1); // "Петрович"
-const patronymicInitial = patronymic.charAt(0).toUpperCase() + "."; //П.
+    const firstName = name.slice(firstSpace + 1, secondSpace); // "Сергей"
+    const firstNameInitial = firstName.charAt(0).toUpperCase() + "."; // С.
 
-const result = formatterSurname + ' ' + firstNameInitial + ' ' + patronymicInitial;
-console.log("Результат: ", result)
+    const patronymic = name.slice(secondSpace + 1); // "Петрович"
+    const patronymicInitial = patronymic.charAt(0).toUpperCase() + "."; //П.
+
+    const result = formatterSurname + ' ' + firstNameInitial + ' ' + patronymicInitial;
+    console.log("Результат: ", result)
+}
+
+
+abbreviateName(" гусева Светлана Дмитриевна "); // "Гусева С. Д."
