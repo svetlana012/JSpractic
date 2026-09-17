956c6a704760993c42d496b18f2478c15403e9bf
Светлана <svetlanaguseva264@gmail.com>
2026-09-17 20:51:34 +0200

    правки

--- script_14.js
+++ script_14.js
@@ -197,6 +197,7 @@ class Calculator {
         }
         if (value === "⌫") {
           this.backspace();
+          this.updateDisplay();
           return;
         }
         if (["+", "−", "*", "/"].includes(value)) {
@@ -254,7 +255,7 @@ class Calculator {
   }
 
   calculate() {
-    if(this.operation || this.previousValue === null) {
+    if(this.operation === null || this.previousValue === null) {
       return
     }
 
